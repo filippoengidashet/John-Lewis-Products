656c9b6e072453e525256da0d2815c157713eeba
Filippo <filippo.eng@gmail.com>
2022-02-08 22:02:03 +0000

    Remooved duplicated files

--- app/src/main/java/com/filippoengidashet/johnlewis/data/model/ProductSpec.kt
+++
@@ -1,8 +0,0 @@
-package com.filippoengidashet.johnlewis.data.model
-
-/**
- * @author Filippo Engidashet
- * @version 1.0.0
- * @since Tue, 08/02/2022 at 18:06.
- */
-class ProductSpec(val name: String, val value: String)
--- app/src/main/java/com/filippoengidashet/johnlewis/data/model/dto/ProductsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.filippoengidashet.johnlewis.data.model.dto
-
-/**
- * @author Filippo Engidashet
- * @version 1.0.0
- * @since Tue, 08/02/2022 at 12:10.
- */
-class ProductsResponse(val products: List<Product>?)
